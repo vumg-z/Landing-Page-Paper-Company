a7624a0ec012ee54a3acee375b7c47f58fd4b890
vumg <ab17100222@gmail.com>
2023-01-22 11:33:59 -0600

    commit before adding tw-elements

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
 
 
 window.onload = () => {
-    let button = document.getElementById("mobile-menu");
-    let iconOpen = document.getElementById("icon_open");
-    let iconClosed = document.getElementById("icon_closed");
-    let menuList = document.getElementById("mobile_menu_list");
-
-    let isMenuOpen = false; 
+    let buttonProducts = document.getElementById("button_products");
+   
     
-    button.onclick = () => {
-        isMenuOpen = true;
-
-        if(isMenuOpen){
-            iconOpen.classList.toggle("hidden");
-            iconClosed.classList.toggle("hidden");
-            menuList.classList.toggle("hidden");
-        }
+    buttonProducts.onclick = () => {
+        window.location.href = "/src/productos.html";
     }
 
 
