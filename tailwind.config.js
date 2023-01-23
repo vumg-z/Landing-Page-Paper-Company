27b5d6761ed0d473a35baea54e2ffc8d14b5cd57
vumg <ab17100222@gmail.com>
2023-01-22 21:34:02 -0600

    before colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js", './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin')
   ]
 }
